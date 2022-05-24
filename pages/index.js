4635bbcf766fde25d19f29a9c65954d5e3590c81
grandpoisson <grandpoisson@gmail.com>
2022-05-24 18:52:48 +0300

    implemented next auth validation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useSession } from 'next-auth/react'
 import Form from '../components/Form'
 
 export default function Home() {
+
+  const { data: session, status } = useSession()
   return (
     <div className={styles.container}>
-      <Form />
+      {session ? (
+      <p>You are logged in!</p> 
+      ): (
+      <p>
+        You are not logged in 😞 <a href='/api/auth/signin'>Login</a>
+        </p>
+        )}
     </div>
   )
 }
